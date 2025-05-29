58b736ca5c531b7722a7825d2db8460a5818bfb7
t2408122 <t2408122@partner.coway.com>
2025-05-29 15:46:24 +0900

    feat: 컨트리뷰션 가이드 작성

--- docs/website/src/app/_meta.ts
+++ docs/website/src/app/_meta.ts
@@ -6,6 +6,24 @@ const meta = {
     type: 'page',
     title: 'Documentation',
   },
+  guide: {
+    type: 'page',
+    title: 'Guide',
+  },
+  lagacy: {
+    type: 'menu',
+    title: 'Legacy',
+    items: {
+      iocare: {
+        title: 'IoCare+ UI Guide',
+        href: 'https://d1t7d82k8r0v04.cloudfront.net/guide/page.html',
+      },
+      necoa: {
+        title: 'Necoa UI Guide',
+        href: 'https://resources.necoa.com/iot-app/pub/guide/page.html',
+      },
+    },
+  },
 };
 
 export default meta;
